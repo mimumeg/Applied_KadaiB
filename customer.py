866e097dc8a506eefbdbc6a7f6888ffc5322c40e
mimumeg <meg.stella555@gmail.com>
2018-11-20 21:19:12 +0900

    名前をフルネームで返すようにコードを記述した。

--- customer.py
+++ customer.py
@@ -7,9 +7,12 @@ class Customer:
         return self.first_name + self.family_name
 
 
+
 if __name__ == "__main__":
     ken = Customer(first_name="Ken", family_name="Tanaka")  # インスタンス化
     ken.full_name()  # "Ken Tanaka" という値を返す
+    print(ken.full_name())
 
-    tom = Customer(first_name="Tom", family_name="Ford").full_name()
+    tom = Customer(first_name="Tom", family_name="Ford")  # インスタンス化
     tom.full_name()  # "Tom Ford" という値を返す
+    print(tom.full_name())
