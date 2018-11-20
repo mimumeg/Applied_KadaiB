bf34bcf30ad4947b28c3d66b1790e66fba412b38
mimumeg <meg.stella555@gmail.com>
2018-11-20 21:06:39 +0900

    First commit

---
+++ customer.py
@@ -0,0 +1,15 @@
+class Customer:
+    def __init__(self, first_name, family_name):
+        self.first_name = first_name
+        self.family_name = family_name
+
+    def full_name(self):
+        return self.first_name + self.family_name
+
+
+if __name__ == "__main__":
+    ken = Customer(first_name="Ken", family_name="Tanaka")  # インスタンス化
+    ken.full_name()  # "Ken Tanaka" という値を返す
+
+    tom = Customer(first_name="Tom", family_name="Ford").full_name()
+    tom.full_name()  # "Tom Ford" という値を返す
