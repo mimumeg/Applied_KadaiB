ef122d1544f2e58190fc9ce9c692c2ffae8d6bdc
mimumeg <meg.stella555@gmail.com>
2018-11-20 22:30:34 +0900

    ageを追加した。

--- customer.py
+++ customer.py
@@ -1,7 +1,8 @@
 class Customer:
-    def __init__(self, first_name, family_name):
+    def __init__(self, first_name, family_name, age):  # 年齢ageを追加
         self.first_name = first_name
         self.family_name = family_name
+        self.age = age  # 年齢ageをインスタンス変数に設定
 
     def full_name(self):
         return self.first_name + self.family_name
@@ -9,10 +10,15 @@ class Customer:
 
 
 if __name__ == "__main__":
-    ken = Customer(first_name="Ken", family_name="Tanaka")  # インスタンス化
+    ken = Customer(first_name="Ken", family_name="Tanaka", age=15)  # インスタンス化, 年齢15を追加
     ken.full_name()  # "Ken Tanaka" という値を返す
-    print(ken.full_name())
+    ken.age  # 15 という値を返す
 
-    tom = Customer(first_name="Tom", family_name="Ford")  # インスタンス化
+    tom = Customer(first_name="Tom", family_name="Ford", age=57)  # インスタンス化、年齢57を追加
     tom.full_name()  # "Tom Ford" という値を返す
-    print(tom.full_name())
+    tom.age  # 57 という値を返す
+
+    # ieyasuを追加
+    ieyasu = Customer(first_name="Ieyasu", family_name="Tokugawa", age=73)
+    ieyasu.age  # 73 という値を返す
+    # print(ieyasu.full_name())
