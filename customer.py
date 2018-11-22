88635da362a016d2d4bcef91e2e8c86342efa81e
mimumeg <meg.stella555@gmail.com>
2018-11-22 21:53:18 +0900

    応用問題のB-5, B-6を解いた。 祖父をプログラミング上で蘇らすことができたw

--- customer.py
+++ customer.py
@@ -11,15 +11,22 @@ class Customer:
         return self.age
 
     def entry_fee(self):
-        if self.age >= 0 and self.age < 20:
+        if self.age <= 3 and self.age >= 0:  # 3歳以下は無料
+            return 0
+        if self.age > 3 and self.age < 20:  # 4歳以上20歳未満は¥1,000
             return 1000
-        if self.age >= 20 and self.age < 65:
+        if self.age >= 20 and self.age < 65:  # 20歳以上65歳未満は¥1,500
             return 1500
-        if self.age >= 65:
+        if self.age >= 65 and self.age < 75:  # 65歳以上は75歳未満は¥1,200
             return 1200
+        if self.age >= 75:
+            return 500
 
     def info_csv(self):
         return f"{self.first_name}, {self.family_name}, {self.age}, {self.entry_fee()}"
+
+    def info_tab(self):
+        return f"{self.first_name}, {self_family_name}, {self.age}, {self.entry_fee()}"
 
 if __name__ == "__main__":
     ken = Customer(first_name="Ken", family_name="Tanaka", age=15)  # インスタンス化, 年齢15を追加
@@ -27,6 +34,7 @@ if __name__ == "__main__":
     ken.entry_fee()  # 1000という値を返す
     # print(f"Fee:¥{ken.entry_fee()}")
     print(ken.info_csv())  # Ken, Tanaka, 15, 1000 を返す
+    print(ken.info_csv())  # Ken    Tanaka   15  1000 を返す
 
     tom = Customer(first_name="Tom", family_name="Ford", age=57)  # インスタンス化、年齢57を追加
     # tom.full_name()  # "Tom Ford" という値を返す
@@ -40,3 +48,11 @@ if __name__ == "__main__":
     # print(ieyasu.display_profile())
     # print(f"Fee:¥{ieyasu.entry_fee()}")  # 1200という値を返す
     print(ieyasu.info_csv())  # Ieyasu, Tokugawa, 73, 1200 を返す
+
+    # 3歳のtaroを追加
+    taro = Customer(first_name="Taro", family_name="Yamada", age=3)  # taroインスタンス化
+    print(taro.info_csv())
+
+    # 86歳のMinoruを追加
+    minoru = Customer(first_name="Minoru", family_name="Tamura", age=86)  # 和製アインシュタインをインスタンス化
+    print(minoru.info_csv())
